46c7be0e3cac1adffbccea4439be959c12a2dbff
cscstudent <cscstudent@SAC2103-038.campus.csudh.edu>
2023-09-02 15:37:28 -0700

    Commit

--- CSC123_1/src/HelloWorld.java
+++ CSC123_1/src/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		//This line prints the following statement "Hello World 2"
 		System.out.println("Hello World 2");
+		//
+		System.out.println("Hello World 3");
 	}
 }
