7572ea3f6df2eb5061d2eee317b03443c017453d
cscstudent <cscstudent@SAC2103-038.campus.csudh.edu>
2023-09-02 15:31:41 -0700

    Commit

---
+++ CSC123_1/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This line prints the following statement "Hello World 2"
+		System.out.println("Hello World 2");
+	}
+}
